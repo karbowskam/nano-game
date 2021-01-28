b653bab90e6d3b43645d92fdec028de2b58f2f9a
Marta Karbowska <karbowana.marta@gmail.com>
2021-01-28 08:06:24 +0100

    Added canvas in JS and painted exapmle green square

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import "../scss/style.scss";
 
-console.log("Hello world");
+var canvas = document.createElement("canvas");
+canvas.width = 800;
+canvas.height = 600;
+document.body.appendChild(canvas);
+
+var c = canvas.getContext("2d");
+c.fillStyle = "green";
+c.fillRect(10, 10, 30, 30);
+document.body.appendChild(canvas);
+
